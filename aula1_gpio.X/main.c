14a417d79069e281422c01c4ff4e85c7b4e2bfdf
Lucas Cechin Mário <lucas.cm.0807@gmail.com>
2026-08-20 11:14:02 -0300

    Atualização do main.c e adição de simulação

--- aula1_gpio.X/main.c
+++ aula1_gpio.X/main.c
@@ -5,11 +5,16 @@
  * Created on August 20, 2026, 9:06 AM
  */
 
-
+#define F_CPU 16000000
 #include <xc.h>
+#include "util/delay.h"
 
 void main(void) {
+    DDRB |= 0b00100000; //define pino PB5 como saída
     while (1){
-        
+        PORTB |= 0b00100000; //atribui nível lógico alto ao pino PB5
+        _delay_ms(100);
+        PORTB &= 0b11011111;
+        _delay_ms(900);
     }
 }
