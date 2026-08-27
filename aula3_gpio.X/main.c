b79266e48b840aacf58cf91d61e76539ef1b1843
Lucas Cechin Mário <lucas.cm.0807@gmail.com>
2026-08-27 11:10:05 -0300

    Introdução à interrupção externa, código main.c com disparo de LED quando em interrupção, juntamente com sua simulação.

---
+++ aula3_gpio.X/main.c
@@ -0,0 +1,45 @@
+/* 
+ * File:   main.c
+ * Author: lucascechinmario
+ *
+ * Created on August 27, 2026, 10:27 AM
+ */
+
+#define F_CPU 16000000
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+
+/*
+ *Configuração da Interrupção Externa (INT0/INT1) 
+ */
+ISR(INT0_vect){ //Função no vetor de interrupção INT0
+    PORTB |= (1<<PORTB1); //Acende led em PB1
+    _delay_ms(100);
+    PORTB &= ~(1<<PORTB1); //Apaga led em PB1
+}
+
+
+void INT_config(){
+    EICRA = (0<<ISC01)|(1<<ISC00); //configura INT0 borda de descida
+    EIMSK = (1<<INT0); //Habilita INT0
+}
+
+void GPIO_config(){
+    DDRB |= (1<<DDB0);//saída
+    DDRB |= (1<<DDB1);//saída
+}
+
+int main(void) {
+    INT_config();
+    GPIO_config();
+    sei(); //Habilita Interrupções globalmente
+    while(1){
+        _delay_ms(100);
+        PORTB ^= (1<<PORTB0);
+    }
+}
+
