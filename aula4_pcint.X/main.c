d827735dc77b05196fae936124d717457be996d1
Lucas Cechin Mário <lucas.cm.0807@gmail.com>
2026-09-03 11:36:35 -0300

    adição da lógica da tecla s, ainda precisa de correção

--- aula4_pcint.X/main.c
+++ aula4_pcint.X/main.c
@@ -36,8 +36,13 @@ void GPIO_incBar(){
     PORTD |= 0b10000000; //aciona bit mais significativo
 }
 
+void GPIO_decBar(){
+    PORTD = PORTD << 1;
+}
+
 
 ISR(PCINT0_vect){
+    //Lógica da tecla W
     uint8_t tCurrentKeyState_w = 0;
     if(PINB & (1<<PINB0) != 0){//testa pino PB0
         //PB0 = 1, tecla 2 não pressionada
@@ -55,6 +60,26 @@ ISR(PCINT0_vect){
     else if(tCurrentKeyState_w == KEY_RELEASED && gKeyState_w == KEY_PRESSED){
         //tecla w acabou de
         gKeyState_w = KEY_RELEASED;
+    }
+    
+    //Lógica da tecla S
+    uint8_t tCurrentKeyState_s = 0;
+    if(PINB & (1<<PINB2) != 0){//testa pino PB0
+        //PB2 = 1, tecla 2 não pressionada
+        tCurrentKeyState_s = KEY_RELEASED;
+    }
+    else{
+        //PB2 = 0, tecla w pressionada
+        tCurrentKeyState_s = KEY_PRESSED;
+    }
+    if(tCurrentKeyState_s == KEY_PRESSED && gKeyState_s == KEY_RELEASED){
+        //tecla s está pressionada
+        gKeyState_s = KEY_PRESSED;
+        GPIO_decBar();
+    }
+    else if(tCurrentKeyState_s == KEY_RELEASED && gKeyState_s == KEY_PRESSED){
+        //tecla s acabou de
+        gKeyState_s = KEY_RELEASED;
     }
     
     PORTC ^= (1<<PORTC0);//seta pino PC0
