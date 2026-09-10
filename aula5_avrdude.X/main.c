317f96393dc9fa7d7bf01d3678f466af4d6ee382
Lucas Cechin Mário <lucas.cm.0807@gmail.com>
2026-09-10 10:00:04 -0300

    Código com script para deslocamento de bits, acendendo leds L0-L7 da bancada PIC-2377

--- aula5_avrdude.X/main.c
+++ aula5_avrdude.X/main.c
@@ -16,14 +16,30 @@
  * 
  */
 
+void GPIO_incBar(){
+    PORTD = PORTD >> 1;
+    PORTD |= 0b10000000;
+}
+
+void GPIO_decBar(){
+    PORTD = PORTD << 1;
+}
+
 void GPIO_config(){
-    DDRB |= (1<<DDB5);//Pino PB5 (13) como saída
+    DDRD |= 0xFF;//Pinos usados pelos LEDs como saída
+    PORTD = 0x00;//Parte com a barra apagada
 }
 
 void main(void){
     GPIO_config();
     while(1){
-        PORTB ^= (1<<PORTB5);//Alterna o estado do LED
-        _delay_ms(500);
+        for (uint8_t i = 0; i < 8; i++) {
+            GPIO_incBar();
+            _delay_ms(500);
+        }
+        for (uint8_t i = 0; i < 8; i++) {
+            GPIO_decBar();
+            _delay_ms(200);
+        }
     }
 }
