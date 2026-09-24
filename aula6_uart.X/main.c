f83715a4c901c86127707241f91dbe4183303ee8
Lucas Cechin Mário <lucas.cm.0807@gmail.com>
2026-09-24 09:25:22 -0300

    Adição da transmissão de letras e palavras pela porta de transmissão, utilizando 'semáforo' como delay entre uma byte e outro

--- aula6_uart.X/main.c
+++ aula6_uart.X/main.c
@@ -11,6 +11,10 @@
 #include <stdlib.h>
 #include <xc.h>
 #include <util/delay.h>
+
+char gMessage[16] = "Mensagem"; //"Null terminated strings" (8 bytes + 1 terminação nula)
+
+
 /*
  * Configuração do módulo USART0 para modo assíncrono, frame de 8 bits, paridade par, BAUD de 9600
  */
@@ -19,22 +23,32 @@ void UART_config(){
     UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(0<<UCSZ02);//Habilita o transmissor UART0, frame de 8 bits
     UCSR0C = (0<<UMSEL01)|(0<<UMSEL00)//Modo assíncrono
             |(1<<UPM01)|(0<<UPM00)//Habilita paridade par
-            |(0<<USBS0)//1 bit de stop
+            |(1<<USBS0)//1 bit de stop
             |(1<<UCSZ01)|(1<<UCSZ00)//frame de 8bits
             |(0<<UCPOL0);//Polaridade do clock
     UBRR0 = 103;//BAUD de 9600, erro de 0,16%
 }
 
 //Envia um byte na USART0
-void UART_send(){
-    UDR0 = 0x53;//Envia um byte exemplo
-    _delay_ms(2);
+void UART_send_byte(uint8_t pByte){
+    //Aguardar espaço no buffer(semaforo))
+    while((UCSR0A & (1<<UDRE0)) == 0);
+    UDR0 = pByte;//Envia um byte exemplo
+}
+
+void UART_send_string(char * pString){
+    char *tMessagePtr = pString;
+    while(*tMessagePtr != 0){
+        UART_send_byte(*tMessagePtr);
+        tMessagePtr++;
+    }
 }
 
 int main(void) {
     UART_config();
     while(1){
-        UART_send();
+        UART_send_string(gMessage);
+        _delay_ms(5);//Tempo de silêncio
     }
 }
 
