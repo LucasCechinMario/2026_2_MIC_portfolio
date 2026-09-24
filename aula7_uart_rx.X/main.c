a17b72a58449ba3b3e95ea03357f462de1508f25
Lucas Cechin Mário <lucas.cm.0807@gmail.com>
2026-09-24 11:39:28 -0300

    Commit final da aula inacabado

--- aula7_uart_rx.X/main.c
+++ aula7_uart_rx.X/main.c
@@ -12,8 +12,13 @@
 #include <xc.h>
 #include <util/delay.h>
 #include "avr/interrupt.h"
+#include <string.h>
 
-char gMessage[16] = "Mensagem"; //"Null terminated strings" (8 bytes + 1 terminação nula)
+#define RX_BUFFER_SIZE 16
+
+uint8_t gMessage[RX_BUFFER_SIZE]; //Buffer global da UART
+uint16_t gRxCounter = 0; //Contagem de bytes recebidos
+uint8_t gMessageReady = 0;
 
 /*
  * Configuração do módulo USART0 para modo assíncrono, frame de 8 bits, paridade par
@@ -27,7 +32,7 @@ void UART_config_rx(uint16_t pBAUD){
             |(1<<USBS0)//1 bit de stop
             |(1<<UCSZ01)|(1<<UCSZ00)//frame de 8bits
             |(0<<UCPOL0);//Polaridade do clock: ignorada
-    UBRR0 = (1000000 / pBAUD) - 1;//BAUD de 9600, erro de 0,16%
+    UBRR0 = (F_CPU / (16UL * pBAUD)) - 1;
 }
 
 void GPIO_config(){
@@ -35,16 +40,16 @@ void GPIO_config(){
 }
 
 ISR(USART_RX_vect){
-    uint8_t tReceivedByte = UDR0;
-    //Tratamento de mensagem recebida
-    if(tReceivedByte == 'M'){
-        PORTC |= (1<<PORTC0); //LED VERDE
+    uint8_t tReceivedByte = UDR0;//Leitura do buffer 
+    gMessage[gRxCounter] = tReceivedByte;
+    gRxCounter++;
+    if(gRxCounter < RX_BUFFER_SIZE - 1){
+        gMessage[gRxCounter] = tReceivedByte;
+        gRxCounter++;
     }
-    else{
-        PORTC |= (1<<PORTC1); //LED VERMELHO
+    if(tReceivedByte == '\n'){
+        gMessageReady = 1;//Flag de mensagem completa
     }
-    _delay_ms(1);
-    PORTC = 0; //Apaga todos os leds
 }
 
 int main(void) {
@@ -52,7 +57,18 @@ int main(void) {
     UART_config_rx(9600);
     sei();//Habilita interrupções globalmente
     while(1){
-        //
+        if(gMessageReady){
+            gMessageReady = 0;
+        }
+        //Tratamento de mensagem recebida
+        if(strcmp((char*)gMessage, "Mensagem\n") == 0){
+            PORTC |= (1<<PORTC0); //LED VERDE
+        }
+        else{
+            PORTC |= (1<<PORTC1); //LED VERMELHO
+        }
+        _delay_ms(10);
+        PORTC = 0; //Apaga todos os leds
     }
 }
 
