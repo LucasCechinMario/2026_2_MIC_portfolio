e5b2d7e86e51cdaf7c57cbfb92891f80448236c1
Lucas Cechin Mário <lucas.cm.0807@gmail.com>
2026-09-24 11:22:27 -0300

    Tratamento de recepção UART via interrupção

--- aula7_uart_rx.X/main.c
+++ aula7_uart_rx.X/main.c
@@ -11,16 +11,17 @@
 #include <stdlib.h>
 #include <xc.h>
 #include <util/delay.h>
+#include "avr/interrupt.h"
 
 char gMessage[16] = "Mensagem"; //"Null terminated strings" (8 bytes + 1 terminação nula)
-
 
 /*
  * Configuração do módulo USART0 para modo assíncrono, frame de 8 bits, paridade par
  */
 void UART_config_rx(uint16_t pBAUD){
     UCSR0A = (0<<U2X0);
-    UCSR0B = (1<<RXEN0)|(0<<TXEN0)|(0<<UCSZ02);//Habilita o receptor UART0, frame de 8 bits
+    UCSR0B = (1<<RXEN0)|(0<<TXEN0)|(0<<UCSZ02)//Habilita o receptor UART0, frame de 8 bits
+            | (1<<RXCIE0);//Habilita interrupção
     UCSR0C = (0<<UMSEL01)|(0<<UMSEL00)//Modo assíncrono
             |(1<<UPM01)|(0<<UPM00)//Habilita paridade par
             |(1<<USBS0)//1 bit de stop
@@ -33,21 +34,25 @@ void GPIO_config(){
     DDRC = (1<<DDC0)|(1<<DDC1); //PC0 e PC1 como saída
 }
 
+ISR(USART_RX_vect){
+    uint8_t tReceivedByte = UDR0;
+    //Tratamento de mensagem recebida
+    if(tReceivedByte == 'M'){
+        PORTC |= (1<<PORTC0); //LED VERDE
+    }
+    else{
+        PORTC |= (1<<PORTC1); //LED VERMELHO
+    }
+    _delay_ms(1);
+    PORTC = 0; //Apaga todos os leds
+}
+
 int main(void) {
     GPIO_config();
     UART_config_rx(9600);
+    sei();//Habilita interrupções globalmente
     while(1){
-        while(UCSR0A & (1<<RXC0) == 0); //Semáforo na flag de recebimento da UART
-        uint8_t tReceivedByte = UDR0;
-        //Tratamento de mensagem recebida
-        if(tReceivedByte == 'M'){
-            PORTC |= (1<<PORTC0); //LED VERDE
-        }
-        else{
-            PORTC |= (1<<PORTC1); //LED VERMELHO
-        }
-        _delay_ms(1);
-        PORTC = 0; //Apaga todos os leds
+        //
     }
 }
 
