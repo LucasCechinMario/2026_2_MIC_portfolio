2f6a5c5846901fa05ae0fddbc080c5950db0faf5
Lucas Cechin Mário <lucas.cm.0807@gmail.com>
2026-09-24 11:04:37 -0300

    código de recepção UART por bytes individuais, o código pisca os leds de acordo com o caractere recebido. Inclui simulação com transmissor da aula6 ligado ao receptor no simulide

--- aula6_uart.X/main.c
+++ aula6_uart.X/main.c
@@ -42,6 +42,7 @@ void UART_send_string(char * pString){
         UART_send_byte(*tMessagePtr);
         tMessagePtr++;
     }
+    UART_send_byte('\n');
 }
 
 int main(void) {
---
+++ aula7_uart_rx.X/main.c
@@ -0,0 +1,53 @@
+/* 
+ * File:   main.c
+ * Author: lucascechinmario
+ *
+ * Created on September 24, 2026, 10:22 AM
+ */
+
+#define F_CPU 16000000
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+#include <util/delay.h>
+
+char gMessage[16] = "Mensagem"; //"Null terminated strings" (8 bytes + 1 terminação nula)
+
+
+/*
+ * Configuração do módulo USART0 para modo assíncrono, frame de 8 bits, paridade par
+ */
+void UART_config_rx(uint16_t pBAUD){
+    UCSR0A = (0<<U2X0);
+    UCSR0B = (1<<RXEN0)|(0<<TXEN0)|(0<<UCSZ02);//Habilita o receptor UART0, frame de 8 bits
+    UCSR0C = (0<<UMSEL01)|(0<<UMSEL00)//Modo assíncrono
+            |(1<<UPM01)|(0<<UPM00)//Habilita paridade par
+            |(1<<USBS0)//1 bit de stop
+            |(1<<UCSZ01)|(1<<UCSZ00)//frame de 8bits
+            |(0<<UCPOL0);//Polaridade do clock: ignorada
+    UBRR0 = (1000000 / pBAUD) - 1;//BAUD de 9600, erro de 0,16%
+}
+
+void GPIO_config(){
+    DDRC = (1<<DDC0)|(1<<DDC1); //PC0 e PC1 como saída
+}
+
+int main(void) {
+    GPIO_config();
+    UART_config_rx(9600);
+    while(1){
+        while(UCSR0A & (1<<RXC0) == 0); //Semáforo na flag de recebimento da UART
+        uint8_t tReceivedByte = UDR0;
+        //Tratamento de mensagem recebida
+        if(tReceivedByte == 'M'){
+            PORTC |= (1<<PORTC0); //LED VERDE
+        }
+        else{
+            PORTC |= (1<<PORTC1); //LED VERMELHO
+        }
+        _delay_ms(1);
+        PORTC = 0; //Apaga todos os leds
+    }
+}
+
